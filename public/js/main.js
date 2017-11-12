84619d361008b40bbc9856c187ff10cff9bfbb26
Ole <yuhyuh@gmail.com>
2017-11-12 23:39:27 +0100

    auto create list

--- public/js/main.js
+++ public/js/main.js
@@ -68,7 +68,7 @@ var outcomeValues, outcomeList, leagueLabel;
 $(document).ready(function() {
     $(document).bind('createList', createList);
 
-    var todaysDay = new Date().getDate();
+    var todaysDay = new Date().getDate() +3;
     filterOnDay = todaysDay;
 
     $('.dayButton').each(function(){
@@ -146,6 +146,7 @@ $(document).on('click', '#leaguelist input:checkbox', function(){
         }
 
         if ($("#leaguelist .league:checked").length == 0) {
+            leagueLabel = "All leagues"
             $(".allSelect").prop('checked',true);
             $("#leagueButton .label").text($(".allSelect").next("label").text())
         }
@@ -260,9 +261,6 @@ $(".dayButton").click(function(){
     setDayFilter(selectedDate);
 });
 
-$("#loadNewData").click(function(){
-    outcomeList.sort('date', { order: "asc" });
-});
 
 function parseDate(input) {
     var parts = input.match(/(\d+)/g);
@@ -372,92 +370,6 @@ function getJsonData() {
                 }
             }
         }
-        console.log(output)
-
         $(document).trigger('createList', output);
     });
-
-
-
-
-    /*var output2 = {events: []};
-
-
-    for (var x = 0; x < 1; x++) {
-        var a = $.getJSON( dataSources[x], function( data ) {
-            var dataLists = [data];
-            for(var i = 0; i < data.events.length; i++) {
-                var item = data.events[i];
-                console.log(item.event.sport)
-                if(item.event.sport=="FOOTBALL") {
-                    var odds1, oddsX, odds2, live, league, homeScore, awayScore;
-                    if(item.betOffers != null && item.betOffers.length > 0 ){
-                        for(var j = 0; j < item.betOffers.length; j++) {
-                            var offer = item.betOffers[j];
-                            if(offer.betOfferType.name == "Match") {
-                                odds1 = offer.outcomes[0].odds;
-                                oddsX = offer.outcomes[1].odds;
-                                odds2 = offer.outcomes[2].odds;
-                            }
-                        }
-                    }
-
-                    var date = new Date(item.event.start);
-                    var currentDate = new Date();
-                    live = (date <= currentDate);
-                    var time = date.getUTCDate() +"/"+(date.getUTCMonth() + 1)+" - "+date.getHours()+":"+(date.getMinutes()<10?'0':'') + date.getMinutes();
-                    if(live) {
-                        homeScore = item.liveData.score.home;
-                        awayScore = item.liveData.score.away;
-                        time = (item.liveData.matchClock.minute<10?'0':'') + item.liveData.matchClock.minute + ":" + (item.liveData.matchClock.second<10?'0':'') + item.liveData.matchClock.second;
-
-                    }
-                    switch (item.event.group) {
-                        case "Premier League":
-                            league = 1;
-                            break;
-                        case "LaLiga":
-                            league = 2;
-                            break;
-                        case "Bundesliga":
-                            league = 3;
-                            break;
-                        case "Serie A":
-                            league = 4;
-                            break;
-                        default:
-                            league = 5;
-                    }
-
-                    output2.events.push({
-                        home : item.event.homeName,
-                        away : item.event.awayName,
-                        sport : (item.event.sport).charAt(0).toUpperCase() + (item.event.sport).slice(1).toLowerCase(),
-                        league : item.event.group,
-                        live : live,
-                        time : time,
-                        odds1Name: item.event.homeName,
-                        odds2Name : item.event.awayName,
-                        odds1: odds1.toString()[0]+"."+odds1.toString()[1]+odds1.toString()[2],
-                        oddsX: oddsX.toString()[0]+"."+oddsX.toString()[1]+oddsX.toString()[2],
-                        odds2: odds2.toString()[0]+"."+odds2.toString()[1]+odds2.toString()[2],
-                        leagueID: league,
-                        homeScore: homeScore,
-                        awayScore: awayScore,
-                        date: date
-                    });
-                    console.log("pushed "+item.event.homeName)
-                };
-            };
-        });
-    }
-
-    $.when(a).done(function(output2){//when all request are successful
-        outcomeList.clear();
-        //var st = JSON.stringify(output2);
-        console.log(output2)
-
-        //createList(output)
-        //outcomeList.add(output.events);
-    });*/
 }
