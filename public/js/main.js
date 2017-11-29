bbe379f1a7549335d97877e5a61a155dd56fc7be
Lillebo <yuhyuh@gmail.com>
2017-11-29 16:01:07 +0100

    added all leagues

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var outcomeOptions = {
-    valueNames: ["home", "away", "sport", "league", { name: 'live', attr: 'live' }, "time", "odds1Name","odds1", "oddsX", "odds2Name", "odds2", "league", "homeScore", "awayScore", "date", "sortDate"],
-    item: '<div class="card-container">\n' +
+    valueNames: ["id", "home", "away", "sport", "league", { name: 'live', attr: 'live' }, "time", "odds1Name","odds1", "oddsX", "odds2Name", "odds2", "league", "homeScore", "awayScore", "date", "sortDate"],
+    item: '<div class="card-container id">\n' +
     '    <div class="card-wrap live">\n' +
     '        <header class="cardheader">\n' +
     '            <div class="card-livelabel started">Live</div>\n' +
@@ -387,43 +387,30 @@ $.when(A,B,C,D,E, F).done(function(aResult, bResult, cResult, dResult, eResult, 
                     homeScore = item.liveData.score.home;
                     awayScore = item.liveData.score.away;
                     time = (item.liveData.matchClock.minute<10?'0':'') + item.liveData.matchClock.minute + ":" + (item.liveData.matchClock.second<10?'0':'') + item.liveData.matchClock.second;
-
-                }
-                switch (item.event.group) {
-                    case "Premier League":
-                        league = 1;
-                        break;
-                    case "LaLiga":
-                        league = 2;
-                        break;
-                    case "Bundesliga":
-                        league = 3;
-                        break;
-                    case "Serie A":
-                        league = 4;
-                        break;
-                    default:
-                        league = 5;
                 }
 
-                output.events.push({
-                    home : item.event.homeName,
-                    away : item.event.awayName,
-                    sport : (item.event.sport).charAt(0).toUpperCase() + (item.event.sport).slice(1).toLowerCase(),
-                    league : item.event.group,
-                    live : live,
-                    time : time,
-                    odds1Name: item.event.homeName,
-                    odds2Name : item.event.awayName,
-                    odds1: odds1.toString()[0]+"."+odds1.toString()[1]+odds1.toString()[2],
-                    oddsX: oddsX.toString()[0]+"."+oddsX.toString()[1]+oddsX.toString()[2],
-                    odds2: odds2.toString()[0]+"."+odds2.toString()[1]+odds2.toString()[2],
-                    leagueID: league,
-                    homeScore: homeScore,
-                    awayScore: awayScore,
-                    date: date,
-                    sortDate: sortDate
-                });
+                if(item.event.sport == "FOOTBALL") {
+                    if(jQuery.inArray(item.event.ID, output) !== -1) {
+                        output.events.push({
+                            id: item.event.ID,
+                            home : item.event.homeName,
+                            away : item.event.awayName,
+                            sport : (item.event.sport).charAt(0).toUpperCase() + (item.event.sport).slice(1).toLowerCase(),
+                            league : item.event.group,
+                            live : live,
+                            time : time,
+                            odds1Name: item.event.homeName,
+                            odds2Name : item.event.awayName,
+                            odds1: odds1.toString()[0]+"."+odds1.toString()[1]+odds1.toString()[2],
+                            oddsX: oddsX.toString()[0]+"."+oddsX.toString()[1]+oddsX.toString()[2],
+                            odds2: odds2.toString()[0]+"."+odds2.toString()[1]+odds2.toString()[2],
+                            homeScore: homeScore,
+                            awayScore: awayScore,
+                            date: date,
+                            sortDate: sortDate
+                        });
+                    }
+                }
             }
         }
         /*        $( "#progressbar" ).progressbar( "value", progress );
